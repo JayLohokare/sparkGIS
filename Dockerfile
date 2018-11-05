dc57a7415136a0ac081571d9b6e419c5e09a6e31
Jay Lohokare <jaylohokare@gmail.com>
2018-11-05 04:12:09 -0500

    Docker now downloads Spark, uses JAVA8 as base img

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,25 @@
-FROM python:3.4-alpine
+FROM java:8
+
 ADD /pythonCode /pythonCode
 WORKDIR /pythonCode
 
-RUN pip install -r requirements.txt
+ENV SPARK_VERSION=2.3.2
+ENV HADOOP_VERSION=2.7
+
+ADD https://raw.githubusercontent.com/guilhem/apt-get-install/master/apt-get-install /usr/bin/
+RUN chmod +x /usr/bin/apt-get-install
+
+RUN apt-get-install -y curl \
+      && wget http://apache.mirror.iphh.net/spark/spark-${SPARK_VERSION}/spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}.tgz \
+      && tar -xvzf spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}.tgz \
+      && mv spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION} spark \
+      && rm spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}.tgz \
+      && cd /
+
+RUN apt-get-install -y python3 python3-setuptools python3-pip
+
+
+RUN pip3 install -r requirements.txt
 
 
 CMD ["gunicorn", "--workers=2", "--bind=0.0.0.0:8000", "server:app"]
--- pythonCode/server.py
+++ pythonCode/server.py
@@ -1,21 +1,41 @@
+from __future__ import print_function # In python 2.7
+
 from flask import Flask
 import subprocess
+import logging
+import os
+
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 app.url_map.strict_slashes = False
 
+def debug(string):
+    app.logger.debug (string)    
 
 @app.route('/', methods=[ 'GET'])
 def hello():
+    # for root, dirs, files in os.walk("."):  
+    #     for filename in files:
+    #         debug(filename)
+
     cmd = ["ls","-l"]
     p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
                             stderr=subprocess.PIPE,
                             stdin=subprocess.PIPE)
     out,err = p.communicate()
-    return out
-
+    
+    out = str(out)
+    out = out.split('\\n')
+    for i in out:
+        debug(i + '\n')
+    s = ""
+    return s
+    
 
 if __name__ == "__main__":
-
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
     #Accessible on http://192.168.99.100 for localhost docker toolbox (windows)
     app.run('0.0.0.0', 8000, debug=True)
